a0b098a38313af65b407acc7826141c3e5674572
sabari venkatesh <sabarigr@gmail.com>
2022-03-28 18:36:01 +0530

    first commit

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const md = new MarkdownIt();
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addFilter('markdown', value => md.renderInline(value));
-
+    eleventyConfig.addPassthroughCopy("images");
     return {
         dir: {
             input: 'src',
